56d415ea3622447146f6a1f2c61da7a52e1d77d9
Vitalii Chechel <vitalii_chechel@epam.com>
2016-10-21 18:42:11 +0300

    init

---
+++ src/main/java/com/mkyong/web/config/ApplicationInitializer.java
@@ -0,0 +1,13 @@
+package com.mkyong.web.config;
+
+import org.springframework.web.WebApplicationInitializer;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
+public class ApplicationInitializer implements WebApplicationInitializer {
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+
+    }
+}
